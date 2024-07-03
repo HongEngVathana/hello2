4359b46568711cb1a03c6c7b38b5802fabe5062e
HongEngVathana <156695257+HongEngVathana@users.noreply.github.com>
2024-07-03 11:09:44 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// const myBtn = document.getElementById("myBnt");
+// const result = document.getElementById("result");
+// myBtn.onclick = function () {
+//   result.textContent = `Good morning o sml`;
+// };
+const myBtn = document.getElementById("myBtn");
+const result = document.getElementById("result");
+
+myBtn.onclick = function () {
+  result.textContent = "Good sfternoon o sml ❤️";
+};
